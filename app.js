379c348db2d646427e248c4d570ea53d9121f4a0
jcamblong <eltrujuy@gmail.com>
2019-11-13 10:59:32 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+
+const data = require('./data/index.js')
+
+// Servidor
+http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+	
+	// Route System
+	switch (req.url) {
+		// Home
+		case '/':
+            res.end('​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.');
+
+			break;
+		// En cartelera
+		case '/en-cartelera':
+			res.end('En cartelera');
+			break;
+		case '/mas-votadas':
+			res.end('Más Votadas');
+			break;
+		case '/sucursales':
+			res.end('Sucursales');
+			break;
+		case '/contacto':
+			res.end('Contacto');
+			break;
+		case '/preguntas-frecuentes':
+			res.end('Preguntas Frecuentes');
+			break;
+		default:
+			res.end('404 not found')
+	}
+}).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
---
+++ data/index.js
@@ -0,0 +1,9 @@
+const faqs = require('./data/faqs.json')
+const movies = require('./data/movies.json')
+const theaters = require('./data/theaters.json')
+
+module.exports = {
+    faqs,
+    movies,
+    theaters,
+}
